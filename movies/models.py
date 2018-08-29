f5268a222ab3c165a27284513660886913feb2c3
skyler <skyler@teambooster.co.kr>
2018-08-29 16:43:34 +0900

    영화 목록 모델 작성 (Movie, Torrent, Genre, MovieGenre)

--- movies/models.py
+++ movies/models.py
@@ -1,1 +1,54 @@
 from django.db import models
+
+
+# POST : title, year, rating, genres, summary 만 필수입력
+class Movie(models.Model):
+    url = models.CharField(max_length=500, blank=True, default='')
+    imdb_code = models.CharField(max_length=200, blank=True, default='')
+    title = models.CharField(max_length=500)
+    title_english = models.CharField(max_length=500, blank=True, default='')
+    title_long = models.CharField(max_length=500, blank=True, default='')
+    slug = models.SlugField(max_length=500, blank=True, default='')
+    year = models.IntegerField()
+    rating = models.FloatField()
+    runtime = models.IntegerField(default=0)
+
+    summary = models.TextField()
+    description_full = models.TextField(blank=True, default='')
+    synopsis = models.TextField(blank=True, default='')
+    yt_trailer_code = models.CharField(max_length=200, blank=True, default='')
+    language = models.CharField(max_length=200, blank=True, default='')
+    mpa_rating = models.CharField(max_length=200, blank=True, default='')
+
+    background_image = models.CharField(max_length=500, blank=True, default='')
+    background_image_original = models.CharField(max_length=500, blank=True, default='')
+    small_cover_image = models.CharField(max_length=500, blank=True, default='')
+    medium_cover_image = models.CharField(max_length=500, blank=True, default='')
+    large_cover_image = models.CharField(max_length=500, blank=True, default='')
+
+    status = models.CharField(max_length=200, blank=True, default='')
+
+    date_uploaded = models.DateTimeField(auto_now=True)
+    date_uploaded_unix = models.IntegerField()
+
+
+class Torrent(models.Model):
+    movie = models.ForeignKey(Movie, on_delete=models.CASCADE)
+    url = models.CharField(max_length=500, blank=True, default='')
+    hash = models.CharField(max_length=500, blank=True, default='')
+    quality = models.CharField(max_length=200, blank=True, default='')
+    seeds = models.IntegerField()
+    peers = models.IntegerField()
+    size = models.CharField(max_length=200, blank=True, default='')
+    size_bytes = models.IntegerField()
+    date_uploaded = models.DateTimeField(auto_now=True)
+    date_uploaded_unix = models.IntegerField()
+
+
+class Genre(models.Model):
+    name = models.CharField(max_length=200)
+
+
+class MovieGenre(models.Model):
+    genre = models.ForeignKey(Genre, on_delete=models.CASCADE)
+    movie = models.ForeignKey(Movie, on_delete=models.CASCADE)
