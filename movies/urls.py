a69599a8bae77876281330f2f09094d247df2ae7
skyler <skyler@teambooster.co.kr>
2018-08-29 22:23:02 +0900

    /movies/list/ 으로 GET 요청시 영화목록 반환

--- movies/urls.py
+++ movies/urls.py
@@ -2,5 +2,6 @@ from django.conf.urls import url, include
 from . import views
 
 urlpatterns = [
-    url(r'yts-movie-list-save/', views.yts_movie_list_save)
+    url(r'yts-movie-list-save/', views.yts_movie_list_save),
+    url(r'list/', views.movie_list_view),
 ]
--- movies/views.py
+++ movies/views.py
@@ -3,6 +3,10 @@ from django.http import HttpResponse
 import requests
 from .models import Movie, Genre, Torrent
 from datetime import datetime
+import json
+from django.forms.models import model_to_dict
+from django.core.paginator import Paginator
+from django.db.models import Q
 
 
 # Create your views here.
@@ -26,7 +30,7 @@ def yts_movie_list_save(request):
     # 영화 저장
     for ml in movie_list:
         if Movie.objects.filter(id=ml.get('id')):
-            pass
+            continue
 
         movie = Movie(
             id=ml.get('id'),
@@ -84,3 +88,84 @@ def yts_movie_list_save(request):
             torrent.save()
 
     return HttpResponse(response.text, content_type='application/json')
+
+
+def movie_list_view(request):
+    time_format = '%Y-%m-%d %H:%M:%S'
+
+    if request.method == 'GET':
+        res = dict()
+
+        movie_list = Movie.objects.filter()
+
+        limit = int(request.GET.get('limit', 20))
+        if limit < 1 or limit > 50:
+            limit = 20
+
+        page = int(request.GET.get('page', 1))
+
+        quality = request.GET.get('quality', 'All')
+        minimum_rating = int(request.GET.get('minimum_rating', 0))
+
+        if 0 <= minimum_rating <= 9:
+            movie_list = movie_list.filter(rating__gte=minimum_rating)
+
+        query_term = request.GET.get('query_term', 0)
+        if query_term:
+            movie_list = movie_list.filter(Q(title__icontains=query_term) | Q(title_english__icontains=query_term)
+                                           | Q(title_long__icontains=query_term) | Q(imdb_code__icontains=query_term))
+
+        genre = request.GET.get('genre', 'All')
+        if genre != 'All':
+            movie_list = movie_list.filter(genres__name__iexact=genre)
+
+        order_by = request.GET.get('order_by', 'desc')
+        if order_by == 'asc':
+            order = ''
+        else:
+            order = '-'
+
+        sort_by = request.GET.get('sort_by', 'date_added')
+        if sort_by in ['title', 'year', 'rating']:
+            movie_list = movie_list.order_by(order+sort_by)
+        else:
+            movie_list = movie_list.order_by(order+'date_uploaded')
+
+        paginator = Paginator(movie_list, limit)
+        page_obj = paginator.page(page)
+        movie_list = page_obj.object_list
+
+        # 응답 만들기
+        res['status'] = 'ok'
+        res['status_message'] = 'Query was successful'
+        res['data'] = {
+            'movie_count': paginator.count,
+            'limit': limit,
+            'page_number': page,
+        }
+        res['movies'] = list()
+
+        for ml in movie_list:
+            movie_dict = model_to_dict(ml)
+            movie_dict['genres'] = [gen.name for gen in ml.genres.all()]
+            movie_dict['date_uploaded'] = datetime.strftime(ml.date_uploaded, time_format)
+
+            movie_dict['torrents'] = list()
+            torrent_list = Torrent.objects.filter(movie=ml)
+            if quality in ['720p', '1080p', '3D']:
+                torrent_list = torrent_list.filter(quality=quality)
+
+            for tl in torrent_list:
+                torrent_dict = model_to_dict(tl)
+                torrent_dict['date_uploaded'] = datetime.strftime(tl.date_uploaded, time_format)
+                del torrent_dict['id']
+                del torrent_dict['movie']
+                movie_dict['torrents'].append(torrent_dict)
+
+            res['movies'].append(movie_dict)
+
+        return HttpResponse(json.dumps(res), content_type='application/json')
+
+    elif request.method == 'POST':
+        pass
+    return HttpResponse({}, content_type='application/json')
